8a50ac2acd0f50b5c4ac7c5cfcf162ac9453ac1b
Justin Shaw <thoushawnotpass@gmail.com>
2020-12-05 19:46:10 -0800

    added speech indicator

---
+++ lib/timer/speech_indicator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:preptime/provider/models/debate_event.dart';
+import 'package:preptime/provider/models/event.dart';
+import 'package:preptime/provider/models/speech.dart';
+import 'package:preptime/provider/models/speech_event.dart';
+import 'package:provider/provider.dart';
+
+/// Returns a row of page indicators.
+class SpeechIndicator extends StatelessWidget {
+  static const EdgeInsetsGeometry _gap = EdgeInsets.symmetric(horizontal: 5.0);
+  static const MainAxisAlignment _alignCenter = MainAxisAlignment.center;
+  static const Duration _fadeDuration = Duration(milliseconds: 500);
+  static const Color _inactiveColor = Colors.black12;
+  static const Color _activeColor = Colors.black38;
+  static const Size _indicatorSize = Size(8.0, 8.0);
+  static const double _radiusOfBorder = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    Event event = context.watch<Event>();
+    if (event is SpeechEvent) return null;
+    List<Speech> speeches = (event as DebateEvent).speeches;
+    return Row(
+      mainAxisAlignment: _alignCenter,
+      children: <Widget>[
+        for (int i = 0; i < speeches.length; i++)
+          _newIndicator(isActive: speeches[i] == event.speech),
+      ],
+    );
+  }
+
+  /// Returns a new indicator with the associated styling.
+  Widget _newIndicator({@required bool isActive}) {
+    return AnimatedContainer(
+      margin: _gap,
+      height: _indicatorSize.height,
+      width: _indicatorSize.width,
+      duration: _fadeDuration,
+      decoration: BoxDecoration(
+        color: isActive ? _activeColor : _inactiveColor,
+        borderRadius: BorderRadius.circular(_radiusOfBorder),
+      ),
+    );
+  }
+}
