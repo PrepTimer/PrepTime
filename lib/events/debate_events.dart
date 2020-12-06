abf036e088074047161c65b500c6a9e667c00b54
Justin Shaw <thoushawnotpass@gmail.com>
2020-12-05 23:32:57 -0800

    renamed the debate events

--- lib/events/debate_events.dart
+++ lib/events/debate_events.dart
@@ -1,3 +1,5 @@
 library debate_events;
 
-part 'lincoln-douglas.dart';
+part 'package:preptime/events/policy.dart';
+part 'package:preptime/events/lincoln_douglas.dart';
+part 'package:preptime/events/public_forum.dart';
