a2ebe8ad4535169fa192f1946cf57dfa2fdf2e31
Justin Shaw <thoushawnotpass@gmail.com>
2020-12-07 22:04:22 -0800

    disabled the timer ring when prep timer is running

---
+++ lib/timer_ring/src/speech_label.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:preptime/provider/models/debate_event.dart';
+import 'package:preptime/provider/models/event.dart';
+import 'package:preptime/provider/models/speech.dart';
+import 'package:provider/provider.dart';
+
+/// The name of the speech currently being given.
+class SpeechLabel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Speech speech = context.watch<Speech>();
+    Event event = context.watch<Event>();
+    bool isDisabled = (event is DebateEvent) && event.isAnyRunning;
+    return Text(
+      speech.name.toUpperCase(),
+      style: TextStyle(
+        fontWeight: FontWeight.w300,
+        color: isDisabled ? Color(0x44FFFFFF) : Color(0x88FFFFFF),
+        fontSize: 18.0,
+      ),
+    );
+  }
+}
--- lib/timer_ring/timer_ring.dart
+++ lib/timer_ring/timer_ring.dart
@@ -1,8 +1,7 @@
-import 'dart:ui';
-
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:preptime/provider/models/event.dart';
+import 'package:preptime/timer_ring/src/clock_label.dart';
+import 'package:preptime/timer_ring/src/speech_label.dart';
 import 'package:provider/provider.dart';
 import 'package:preptime/provider/models/debate_event.dart';
 import 'package:preptime/provider/models/speech.dart';
@@ -19,43 +18,18 @@ class _TimerRingState extends State<TimerRing> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     context.watch<Speech>().initController(this);
-    Speech speech = context.watch<Speech>();
     return Align(
       alignment: FractionalOffset.center,
       child: AspectRatio(
         aspectRatio: 1.0,
         child: Stack(
           children: <Widget>[
-            /// The large, circular ring animation
-            Positioned.fill(
-              child: RingPainter(),
-            ),
-
-            /// The large, white clock text (eg. 00:00.0)
-            Container(
-              alignment: FractionalOffset.center,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: AutoSizeText(
-                speech.timeRemaining,
-                style: Theme.of(context).textTheme.headline1,
-                maxLines: 1,
-              ),
-            ),
-
-            /// The name of the speech being given.
+            RingPainter(),
+            ClockLabel(),
             Align(
               alignment: FractionalOffset(0.5, 0.69),
-              child: Text(
-                speech.name.toUpperCase(),
-                style: const TextStyle(
-                  fontWeight: FontWeight.w300,
-                  color: Color(0x88FFFFFF),
-                  fontSize: 18.0,
-                ),
-              ),
+              child: SpeechLabel(),
             ),
-
-            /// Dots indicating which speech is active (debate only).
             if (context.watch<Event>() is DebateEvent)
               Align(
                 alignment: FractionalOffset(0.5, 0.8),
