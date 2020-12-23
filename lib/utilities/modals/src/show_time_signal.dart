56c5c28c2c83e3446bec1da185c4b21afcfd4886
Justin Shaw <thoushawnotpass@gmail.com>
2020-12-23 15:42:41 -0800

    added assets to pubspec yaml

--- lib/utilities/modals/src/show_time_signal.dart
+++ lib/utilities/modals/src/show_time_signal.dart
@@ -1,1 +1,12 @@
 part of modals;
+
+class ShowTimeSignal {
+  static void five() {}
+  static void four() {}
+  static void three() {}
+  static void two() {}
+  static void one() {}
+  static void fist() {}
+  static void thirty() {}
+  static void fifteen() {}
+}
